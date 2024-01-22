b7b0d17cc3102124a00d714d9c68c8fe4dbe282a
Beregszászy Gyula <gyula.beregszaszy@gmail.com>
2024-01-22 20:46:36 +0100

    Connect to the rabbbitmq
    Signed-off-by: Beregszászy Gyula <gyula.beregszaszy@gmail.com>

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,11 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
 func main() {
 	// try to connect to rabbitmq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+	log.Println("Connected to RabbitMQ!")
 
 	// start listenning for messages
 
 	// create consumer
 
 	// watch the queue and consume events
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready: %s", err)
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("Backing off for %s", backOff)
+		time.Sleep(backOff)
+		continue
+	}
+	return connection, nil
 }
